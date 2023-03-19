32eb35c2d2f14918104a28f545cf724bba433fe6
revertux <revertux@gmail.com>
2023-03-19 21:00:18 +0100

    Remove unused code and correct formatting

--- svelte/pure/src/routes/posts/Post.svelte
+++ svelte/pure/src/routes/posts/Post.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-    export let id: string
-    export let title: string
-    export let body: string
+	export let id: string;
+	export let title: string;
+	export let body: string;
 </script>
 
 <div class="post">
-    <a href={`posts/${id}`}>
-        <div>{title}</div>
-        <div>{body}</div>
-    </a>
+	<a href={`posts/${id}`}>
+		<div>{title}</div>
+		<div>{body}</div>
+	</a>
 </div>
 
 <style>
-    .post {
-        border-radius: 16px;
-        background-color: #30373c;
-        padding: 8px;
-    }
+	.post {
+		border-radius: 16px;
+		background-color: #30373c;
+		padding: 8px;
+	}
 
-    .post:hover {
-        cursor: pointer;
-        background-color: #242a2d;
-    }
+	.post:hover {
+		cursor: pointer;
+		background-color: #242a2d;
+	}
 </style>
